2128f8cbf6759453b679b07158489cd4a344c821
Nicolas Van Wallendael <nicolas.vanwallendael@student.uclouvain.be>
2014-10-12 13:30:25 +0200

    [Initial] commit -> AIMA + maseColect.py + Instances

---
+++ mazeCollect.py
@@ -0,0 +1,39 @@
+##                                                                ##
+##  mazeCollect.py                                                ##
+##  The Koutack Problem : Assigment 1                             ##
+##                                                                ##
+##  INGI2261                                                      ##
+##                                                                ##
+##  Created by Nicolas Van Wallendael && Benoit Dardenne          ##
+##                                                                ##
+
+from itertools import chain
+from math import ceil
+from search import *
+
+
+######################  State class  ###############################
+
+#
+#
+#
+#
+#
+class State :
+
+
+######################  Implement the search #######################
+
+class Koutack(Problem):
+
+
+###################### Launch the search #########################
+problem=Koutack(sys.argv[1])
+
+"""
+#example of print
+path = node.path()
+path.reverse()
+for n in path:
+    print(n.state)
+"""
